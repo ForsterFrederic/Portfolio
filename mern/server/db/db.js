f04e8c9542f848a057122a21d82230078c6da584
ForsterFrederic <frederic.forster@epitech.eu>
2024-06-08 01:43:02 +0200

    test 20

--- mern/server/db/db.js
+++ mern/server/db/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/PortfolioDB';
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/PortfolioDB';
+const MONGODB_URI = 'mongodb+srv://portfolio:Frederic137*@cluster24753.2uvzwvc.mongodb.net/portfolio';
+
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
