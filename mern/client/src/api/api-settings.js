757c5f02946817e9ae0fb5fe5e1e67502cde1ec7
ForsterFrederic <frederic.forster@epitech.eu>
2024-06-08 02:32:47 +0200

    test 23

--- mern/client/src/api/api-settings.js
+++ mern/client/src/api/api-settings.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const backendApiUrl = process.env.BACKEND_API_URL || 'http://localhost:3001/api';
-console.log(process.env.BACKEND_API_URL)
+// const backendApiUrl = process.env.BACKEND_API_URL || 'http://localhost:3001/api';
+const backendApiUrl = 'https://frederic-forster.com/api';
+
 const api = axios.create({
 
     baseURL: backendApiUrl
