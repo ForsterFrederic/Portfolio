b4bd0616a2843cd84cb50e1f203cfa01816c4753
ForsterFrederic <frederic.forster@epitech.eu>
2024-06-10 00:01:01 +0200

    test 21

--- mern/client/src/pages/General/Developpment.js
+++ mern/client/src/pages/General/Developpment.js
@@ -8,7 +8,7 @@ export default function Developpment() {
     useEffect(() => {
         const testServer = async () => {
             try {
-                const response = await axios.get('https://frederic-forster.com:5000/');
+                const response = await axios.get('https://frederic-forster.com/');
                 setResponse(response.data);
             } catch (error) {
                 setResponse("Failed to test the server.");
