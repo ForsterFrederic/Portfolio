8163b3ee0879ff3c719d087ec6c467ee874b9a9c
ForsterFrederic <frederic.forster@epitech.eu>
2024-06-24 17:45:33 +0200

    Developpment card

--- mern/server/db/db.js
+++ mern/server/db/db.js
@@ -4,8 +4,10 @@ const mongoose= require('mongoose');
 const IS_PROD = process.env.IS_PROD;
 const MONGODB_URI = process.env.MONGODB_URI;
 const PROD_MONGODB_URI = process.env.PROD_MONGODB_URI;
-// const URI = (IS_PROD === "TRUE" ? PROD_MONGODB_URI : MONGODB_URI) || 'mongodb://localhost:27017/PortfolioDB';
-const URI = 'mongodb+srv://portfolio:Frederic137*@cluster24753.2uvzwvc.mongodb.net/portfolio';
+const URI = (IS_PROD === "TRUE" ? PROD_MONGODB_URI : MONGODB_URI) || 'mongodb://localhost:27017/PortfolioDB';
+// const URI = 'mongodb+srv://portfolio:Frederic137*@cluster24753.2uvzwvc.mongodb.net/portfolio';
+
+console.log(URI)
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
