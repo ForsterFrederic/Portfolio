32911ecfca2b9fa65f2db024a3b645c3cf57169c
ForsterFrederic <frederic.forster@epitech.eu>
2024-07-06 17:18:32 +0200

    Developpment card

--- mern/client/src/api/api-settings.js
+++ mern/client/src/api/api-settings.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL || "https://frederic-forster.com/api";
 const BACKEND_API_URL = "https://frederic-forster.com/api";
 console.log(BACKEND_API_URL)
+console.log(process.env.REACT_APP_BACKEND_API_URL)
 const api = axios.create({
     baseURL: BACKEND_API_URL
 });
