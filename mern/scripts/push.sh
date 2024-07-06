3948163af1cb65a5fca40e8acce393bf71512d7b
ForsterFrederic <frederic.forster@epitech.eu>
2024-07-06 17:47:17 +0200

    test

--- mern/scripts/push.sh
+++ mern/scripts/push.sh
@@ -11,6 +11,20 @@ fi
 REMOTE="hostinger-vps"
 BRANCH=$(git branch --show-current)
 
+# Function to set IS_PROD value
+set_is_prod() {
+    local value=$1
+    if grep -q '^IS_PROD=' .env; then
+        sed -i "s/^IS_PROD=.*/IS_PROD=$value/" .env
+    else
+        echo "IS_PROD=$value" >> .env
+    fi
+}
+
+# Set IS_PROD to TRUE
+echo "Setting IS_PROD to TRUE"
+set_is_prod "TRUE"
+
 # Add all changes, commit, and push
 git add . && git commit -m "$1" && git push $REMOTE $BRANCH
 
@@ -20,3 +34,7 @@ if [ $? -eq 0 ]; then
 else
     echo "Error: Failed to push changes to $REMOTE/$BRANCH."
 fi
+
+# Set IS_PROD back to FALSE
+echo "Setting IS_PROD back to FALSE"
+set_is_prod "FALSE"
