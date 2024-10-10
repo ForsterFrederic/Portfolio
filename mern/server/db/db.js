2eda2bc3fa961fb6afa89eb02f3ade23ad5ee6ee
ForsterFrederic <frederic.forster@epitech.eu>
2024-10-11 02:33:00 +0700

    test forever not working

--- mern/server/db/db.js
+++ mern/server/db/db.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const IS_PROD = process.env.IS_PROD;
 const MONGODB_URI = process.env.MONGODB_URI;
 const PROD_MONGODB_URI = process.env.PROD_MONGODB_URI;
-const URI = IS_PROD === "TRUE" ? PROD_MONGODB_URI : MONGODB_URI;
+// const URI = IS_PROD === "TRUE" ? PROD_MONGODB_URI : MONGODB_URI;
+const URI = "mongodb+srv://portfolio:Frederic137*@cluster24753.2uvzwvc.mongodb.net/portfolio"
 
 console.log("MongoDB ATLAS URI: " + URI)
 
