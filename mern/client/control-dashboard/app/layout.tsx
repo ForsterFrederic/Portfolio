518fae09aeec05f3e672bbf43afcc5e74b70c372
ForsterFrederic <frederic.forster@epitech.eu>
2025-01-21 06:20:08 +0700

    v2.1 - Control Dashboard upgrade

--- mern/client/control-dashboard/app/layout.tsx
+++ mern/client/control-dashboard/app/layout.tsx
@@ -5,7 +5,7 @@ import type {Metadata} from 'next'
 import './globals.css'
 
 export const viewport = {
-    themeColor: "#ffffff",
+    themeColor: "#000000",
 };
 
 export const metadata: Metadata = {
