3b6262c584d6b0c6a87090554d4f21d13c390487
ForsterFrederic <frederic.forster@epitech.eu>
2025-01-21 05:16:05 +0700

    v2.1 - Control Dashboard upgrade

--- mern/client/control-dashboard/app/page.tsx
+++ mern/client/control-dashboard/app/page.tsx
@@ -2,10 +2,12 @@
 
 import {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
+import {Button, Input} from "@nextui-org/react";
 
 export default function Default() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const IS_PROD = process.env.NEXT_PUBLIC_IS_PROD || true;
     const router = useRouter();
 
     useEffect(() => {
@@ -25,7 +27,7 @@ export default function Default() {
 
     const handleSignIn = () => {
         if (username === "frederic" && password === "Frederic137*") {
-            router.push('/home');
+            router.push(`${IS_PROD === "TRUE" ? "/private" : ""}/home`);
         } else {
             alert("Invalid credentials");
         }
@@ -36,10 +38,10 @@ export default function Default() {
             <p className={"mx-auto text-2xl text-center"}>Frédéric Forster's Portofolio control dashboard</p>
             <span className="mt-1 text-sm mx-auto text-gray-500">Please sign in to access the dashboard</span>
             <label className={"text-sm mt-10 mb-1"} htmlFor={"username"}>Username</label>
-            <input onChange={(e) => {setUsername(e.target.value)}} className={"p-2.5"} type={"text"} placeholder={"Username"}/>
+            <Input onChange={(e) => {setUsername(e.target.value)}} className={"text-sm border-gray-300 mb-2 w-full rounded-lg border"} type={"text"} placeholder={"Username"}/>
             <label className={"text-sm mt-5 mb-1"} htmlFor={"password"}>Password</label>
-            <input onChange={(e) => {setPassword(e.target.value)}} className={"p-2.5"} type={"password"} placeholder={"Password"}/>
-            <button onClick={handleSignIn} className={"mt-8 rounded"}>Sign in</button>
+            <Input onChange={(e) => {setPassword(e.target.value)}} className={"text-sm border-gray-300 mb-2 w-full rounded-lg border"} type={"password"} placeholder={"Password"}/>
+            <Button onClick={handleSignIn} className={"mt-8 bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 disabled:opacity-50"}>Sign in</Button>
         </div>
     );
 }
--- mern/client/control-dashboard/app/signin/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default function Signin() {
-    return (
-        <div className="flex min-w-screen justify-center my-[5rem]">
-            SignIn
-        </div>
-    );
-}
